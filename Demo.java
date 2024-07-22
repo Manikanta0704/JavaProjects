9dff86024b31f0617760cd42788f957a6a866dcf
manikanta <manikantaswamy338@gmail.com>
2024-07-22 22:40:25 +0530

    Demo class committed

---
+++ Demo.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Demo {
+	public static void main(String[] args) throws Exception {
+		String sql="select where id=1";
+		String url="jdbc:postgreasql://localhost:5432/employee_db";
+		String user="postgres";
+		String password="1234";
+		
+		Connection con=DriverManager.getConnection(url, user, password);
+		Statement st=con.createStatement();
+		st.executeQuery(sql);
+		
+	}
+
+}
